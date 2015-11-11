661ac47f373210752a5f03bf0f93135e977edd06
Peter Juras <peter.juras@mobilexag.de>
2015-11-11 10:52:19 +0100

    Remove toString() check in logger unit test

--- test/unit/loggers/logger.js
+++ test/unit/loggers/logger.js
@@ -3,16 +3,13 @@
 
 import { expect } from 'chai';
 import Log4js from '../../../src/index';
-import Logger from '../../../src/logger';
 
 describe('Logger', () => {
   it('returns default logger', () => {
-    expect(Log4js.getLogger().toString()).to.equal(new Logger('[default]').toString());
-    expect(Log4js.loggers['[default]'].toString()).to.equal(Log4js.getLogger().toString());
+    expect(Log4js.getLogger()).to.equal(Log4js.loggers['[default]']);
   });
 
   it('get logger method', () => {
     expect(Log4js.getLogger('category')).to.be.ok;
-    expect(Log4js.getLogger('category').toString()).to.equal(new Logger('category').toString());
   });
 });
